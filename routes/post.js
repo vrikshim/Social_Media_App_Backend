c9f3f8e2d931c500106696bd9c5ff1e876d1a559
Tanishk Goyal <goyaltanishk11112002@gmail.com>
2023-08-20 15:09:39 +0530

    updated

--- routes/post.js
+++ routes/post.js
@@ -9,9 +9,7 @@ router
 .get(isAuthenticated,likeAndUnlikePost)
 .put(isAuthenticated,updateCaption)
 .delete(isAuthenticated,deletePost)
-// this is something we are destined to belong to in the mine field 
-// we are looking for something good in the vicinity f
-// this is not something we are use to do so in the life we are given by the god s
+
 router.route("/posts").get(isAuthenticated,getPostOfFollowing)
 
 router.route("/post/comment/:id").put(isAuthenticated,commentOnPost).delete(isAuthenticated,deleteComment)
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const app=require("./app");
 const {connectDatabase} =require("./config/database")
 const cloudinary=require("cloudinary")
 connectDatabase();
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.COUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.COUDINARY_API_SECRET,
+// });
 app.listen(process.env.PORT,()=>{
     console.log(`server is listening on the port ${process.env.PORT}`)
+    
 })
